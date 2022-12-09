a2d5c773a0d1b012eb945b7db808a4c3016a61dc
Abhisekh <abhisekhkayasth@gmail.com>
2022-12-09 13:19:26 +0545

    Made 16*16 square grid

---
+++ app.js
@@ -0,0 +1,5 @@
+for(var i =0; i<256; i++){
+    const container = document.querySelector('.container')
+    const divContent = document.createElement('div')
+    container.appendChild(divContent)
+}
