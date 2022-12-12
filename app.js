f5ede8a37b7f7d4a9440115acc2dcd32ffc7ac8d
Abhisekh <abhisekhkayasth@gmail.com>
2022-12-12 23:34:13 +0545

    Added event delegation so that whenever your mouse passes over them the grid divs change colors leavig a pixelated trail

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-for(var i =0; i<256; i++){
-    const container = document.querySelector('.container')
-    const divContent = document.createElement('div')
-    container.appendChild(divContent)
+const container = document.querySelector('.container')
+const grid = document.createElement('div')
+grid.classList.add('grid')
+container.appendChild(grid)
+
+for(var i =0; i<256; i++){  
+    const grid_Item = document.createElement('div')
+    grid_Item.classList.add('grid_Item')
+    grid.appendChild(grid_Item)
 }
+
+//Using event delegation to target children of the grid
+grid.addEventListener("mouseover", function (e){
+    // Adding the "active" class to only divs with a "grid_Item" class
+    if (e.target.matches(".grid_Item")){
+        e.target.classList.add("active");
+    }
+})
