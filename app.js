72ca7bd745e3fc91bd79b9377e3fae57ea77dc01
Abhisekh <abhisekhkayasth@gmail.com>
2022-12-22 00:49:56 +0545

    User Data Integration pending

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ const grid = document.createElement('div')
 grid.classList.add('grid')
 container.appendChild(grid)
 
-for(var i =0; i<256; i++){  
+let gridSize = '20';
+
+// Adding event listner to the button
+document.querySelector(".button").addEventListener('click', buttonClick);
+function buttonClick(){
+      gridSize =prompt("Enter the square per side: ");
+    //  for(var i =0; i<gridSize*gridSize; i++){  
+    //     const grid_Item = document.createElement('div')
+    //     grid_Item.classList.add('grid_Item')
+    //     grid.appendChild(grid_Item)
+    // }
+}
+
+for(var i =0; i<gridSize*gridSize; i++){  
     const grid_Item = document.createElement('div')
     grid_Item.classList.add('grid_Item')
+    grid_Item.remo
     grid.appendChild(grid_Item)
 }
 
+
 //Using event delegation to target children of the grid
 grid.addEventListener("mouseover", function (e){
-    // Adding the "active" class to only divs with a "grid_Item" class
+    // Adding the "active" class to only divs which  mouse hovers
     if (e.target.matches(".grid_Item")){
         e.target.classList.add("active");
     }
 })
+console.log(gridSize)
