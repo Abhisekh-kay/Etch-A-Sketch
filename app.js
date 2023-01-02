e917f5c1596377f6f079ae4f006469d0435a920a
Diken <dikenmaharjan10@gmail.com>
2023-01-02 12:33:56 +0545

    feat: change grid size as entered by the user

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 const container = document.querySelector('.container')
-const grid = document.createElement('div')
-grid.classList.add('grid')
-container.appendChild(grid)
 
-let gridSize = '16';
+
+
+
+let gridSize = 16;
 
 // Adding event listner to the button
 document.querySelector(".button").addEventListener('click', buttonClick);
 function buttonClick(){
       gridSize =prompt("Enter the square per side: ");
-    //  for(var i =0; i<gridSize*gridSize; i++){  
-    //     const grid_Item = document.createElement('div')
-    //     grid_Item.classList.add('grid_Item')
-    //     grid.appendChild(grid_Item)
-    // }
+      addGrid(gridSize)
 }
 
-for(var i =0; i<gridSize*gridSize; i++){  
-    const grid_Item = document.createElement('div')
-    grid_Item.classList.add('grid_Item')
-    grid_Item.remo
-    grid.appendChild(grid_Item)
+addGrid(gridSize)
+
+function addGrid(gridSize){
+    container.innerHTML = ''
+    for(var i =0; i<gridSize; i++){  
+        const grid_row = document.createElement('div')
+        grid_row.classList.add('grid_Row')
+        container.appendChild(grid_row)
+        for (var j = 0;j<gridSize;j++){
+            const grid_Item = document.createElement('div')
+            grid_Item.classList.add('grid_Item')
+            grid_row.appendChild(grid_Item)
+        }
+    }
 }
+
+
 
 
 //Using event delegation to target children of the grid
-grid.addEventListener("mouseover", function (e){
+container.addEventListener("mouseover", function (e){
     // Adding the "active" class to only divs which  mouse hovers
     if (e.target.matches(".grid_Item")){
         e.target.classList.add("active");
     }
 })
-console.log(gridSize)
